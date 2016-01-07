89b8e34ce88b0c520ffa4ddc00d0c24c7441e69b
travisgillespie <tgilles34@hotmail.com>
2016-01-07 09:23:43 -0600

    added table details for auto layout project

--- PackingList/ViewController.swift
+++ PackingList/ViewController.swift
@@ -9,34 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //MARK: IB outlets
     
     @IBOutlet var tableView: UITableView!
     @IBOutlet var buttonMenu: UIButton!
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet weak var menuHeightConstraint: NSLayoutConstraint!
     
     //MARK: further class variables
     
-
+    var slider: HorizontalItemList!
+    var isMenuOpen = false
+    var items: [Int] = [5, 6, 7]
     
     //MARK: class methods
     
     @IBAction func actionToggleMenu(sender: UIButton){
-        
+        isMenuOpen = !isMenuOpen
+        menuHeightConstraint.constant = isMenuOpen ? 200.0 : 60.0
+        titleLabel.text = isMenuOpen ? "Select Item" : "Packing List"
     }
     
-
+    func showItem(index: Int) {
+        print("tapped item \(index)")
+    }
 }
 
-
+let itemTitles = ["Icecream money", "Great weather", "Beach ball", "Swim suit for him", "Swim suit for her", "Beach games", "Ironing board", "Cocktail mood", "Sunglasses", "Flip flops"]
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     //MARK: View Controller methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.tableView?.rowHeight = 54.0
     }
     
     //MARK: Table View methods
@@ -46,13 +54,19 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return items.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
-
+        cell.accessoryType = .None
+        cell.textLabel?.text = itemTitles[items[indexPath.row]]
+        cell.imageView?.image = UIImage(named: "summericons_100px_0\(items[indexPath.row]).png")
         return cell
     }
     
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        showItem(items[indexPath.row])
+    }
 }
