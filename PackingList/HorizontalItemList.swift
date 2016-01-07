cb13a1ef12db96b492fef00f86d0bb4a531e4feb
travisgillespie <tgilles34@hotmail.com>
2016-01-07 08:44:54 -0600

    added auto layout project

---
+++ PackingList/HorizontalItemList.swift
@@ -0,0 +1,67 @@
+//
+//  HorizontalItemList.swift
+//  PackingList
+//
+//  Created by Travis Gillespie on 12/27/15.
+//  Copyright © 2015 Travis Gillespie. All rights reserved.
+//
+
+import UIKit
+
+// A scroll view, which loads all 10 images, and has a callback
+// for when the user taps on one of the images
+
+class HorizontalItemList: UIScrollView {
+    
+    var didSelectItem: ((index: Int) -> ())?
+    
+    let buttonWidth: CGFloat = 60.0
+    let padding: CGFloat = 10.0
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    convenience init(inView: UIView) {
+        let rect = CGRect(x: inView.bounds.width, y: 120.0, width: inView.frame.width, height: 80.0)
+        self.init(frame: rect)
+        
+        self.alpha = 0.0
+        
+        for var i = 0; i<10; i++ {
+            let image = UIImage(named: "summericons_100px_0\(i).png")
+            let imageView = UIImageView(image: image)
+            imageView.center = CGPoint(x: CGFloat(i) * buttonWidth + buttonWidth/2, y: buttonWidth/2)
+            imageView.tag = i
+            imageView.userInteractionEnabled = true
+            addSubview(imageView)
+            
+            let tap = UITapGestureRecognizer(target: self, action: Selector("didTapImage:"))
+            imageView.addGestureRecognizer(tap)
+        }
+        
+        contentSize = CGSize(width: padding * buttonWidth, height: buttonWidth + 2*padding)
+    }
+    
+    func didTapImage(tap: UITapGestureRecognizer) {
+        didSelectItem?(index: tap.view!.tag)
+    }
+    
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        
+        if superview == nil {
+            return
+        }
+        
+        UIView.animateWithDuration(1.0, delay: 0.01, usingSpringWithDamping: 0.5, initialSpringVelocity: 10.0, options: .CurveEaseIn,
+            animations: {
+                self.alpha = 1.0
+                self.center.x -= self.frame.size.width
+            }, completion: nil)
+    }
+}
