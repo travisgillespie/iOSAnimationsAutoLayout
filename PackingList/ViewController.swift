e652cb4b48e91d486c465a3e7de7078b20ff7c4f
travisgillespie <tgilles34@hotmail.com>
2016-01-07 08:59:26 -0600

    added packing list details for auto layout project

--- PackingList/ViewController.swift
+++ PackingList/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //MARK: IB outlets
     
     @IBOutlet var tableView: UITableView!
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
     
     //MARK: further class variables
     
-
+    var slider: HorizontalItemList!
+    var isMenuOpen = false
+    var items: [Int] = [5, 6, 7]
     
     //MARK: class methods
     
@@ -26,17 +28,20 @@ class ViewController: UIViewController {
         
     }
     
-
+    func showItem(index: Int) {
+        print("tapped item \(index)")
+    }
 }
 
-
+let itemTitles = ["Icecream money", "Great weather", "Beach ball", "Swim suit for him", "Swim suit for her", "Beach games", "Ironing board", "Cocktail mood", "Sunglasses", "Flip flops"]
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     //MARK: View Controller methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.tableView?.rowHeight = 54.0
     }
     
     //MARK: Table View methods
@@ -46,13 +51,19 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return items.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
-
+        cell.accessoryType = .None
+        cell.textLabel?.text = itemTitles[items[indexPath.row]]
+        cell.imageView?.image = UIImage(named: "summericons_100px_0\(items[indexPath.row]).png")
         return cell
     }
     
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        showItem(items[indexPath.row])
+    }
 }
